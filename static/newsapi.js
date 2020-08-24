21c27bb7fa5aee89e511cfdf1b7b0a5b0d8e5832
Michelle Macaraeg <macaraegm@gmail.com>
2020-08-24 16:07:52 -0700

    Added save button to article - newsapi.js

--- static/newsapi.js
+++ static/newsapi.js
@@ -77,6 +77,47 @@ $(document).ready(function(){
     });
   }
 
+  //passed in news result from the server and renders output
+  let renderNews = function(news) {
+
+    let output = "";
+    let latestNews = news.articles;
+
+    for(var i in latestNews){
+      output +=`
+        <div class="col l6 m6 s12">
+            <h4>${latestNews[i].title}</h4>
+            <img src="${latestNews[i].urlToImage}" class="responsive-img">
+            <p>${latestNews[i].description}</p>
+            <p>${latestNews[i].content}</p>
+            <p>Published on: ${latestNews[i].publishedAt}</p>
+            <a href="${latestNews[i].url}" class="btn">Read more</a>
+
+          <form>
+            <input type="hidden" name="title" value=${latestNews[i].title}>
+            <input type="hidden" name="urlToImage" value=${latestNews[i].urlToImage}>
+            <input type="hidden" name="description" value=${latestNews[i].description}>
+            <input type="hidden" name="content" value=${latestNews[i].content}>
+            <input type="hidden" name="publishedAt" value=${latestNews[i].publishedAt}>
+            <input type="hidden" name="url" value=${latestNews[i].url}>
+            <a class="btn" id="btn_${i}">Save Article</a>
+          </form>
+        </div>
+        
+      `;
+    }
+
+      // spits out html output for our div id #newsResults
+    if(output !== ""){
+      $("#newsResults").html(output);
+    }
+
+    //after search, response if no news is available
+    else{
+      let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>This news isn't available. Sorry about that.<br>Try searching for something else </div>`;
+        $("#newsResults").html(noNews);
+    }
+  }
 
   // MVP - search engine component
   $("#searchbtn").on("click",function(e){
@@ -84,59 +125,33 @@ $(document).ready(function(){
       
       //let query = url + query -> news output
       let query = $("#searchquery").val();
-      let url = ('/api/newsapi/everything?keyword='+query)
+      let url = ('/api/newsapi/everything?keyword=' + query)
 
+      //if statement to determine loader response, after 'search' click
       if(query !== ""){
         
         $.ajax({
            
+          //passing through news api URL, ajax call, get request
           url: url,
           method: "GET",
           dataType: "json",
           
+          //show loader when searching for news
           beforeSend: function(){
             $("#loader").show();
           },
           
+          //hide loader once newsResults appears
           complete: function(){
             $("#loader").hide();
           },
-          
-          success: function(news){
-            let output = "";
-            let latestNews = news.articles;
-            
-            for(var i in latestNews){
-              output +=`
-                <div class="col l6 m6 s12">
-                <h4>${latestNews[i].title}</h4>
-                <img src="${latestNews[i].urlToImage}" class="responsive-img">
-                <p>${latestNews[i].description}</p>
-                <p>${latestNews[i].content}</p>
-                <p>Published on: ${latestNews[i].publishedAt}</p>
-                <a href="${latestNews[i].url}" class="btn">Read more</a>
-                </div>
-              `;
-            }
-            
-            if(output !== ""){
-              $("#newsResults").html(output);
-               M.toast({
-                html: "There you go, nice reading",
-                classes: 'green'
-              });
-              
-            }else{
-              let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>This news isn't available. Sorry about that.<br>Try searching for something else </div>`;
-               $("#newsResults").html(noNews);
-              M.toast({
-                html: "This news isn't available",
-                classes: 'red'
-              });
-            }
-            
-          },
-          // if user encounters an error during keyword search
+
+          //if ajax call is successful, return json object from the news server
+          success: renderNews,
+
+
+          // if user encounters an error during keyword search (i.e. 404)
           error: function(){
              let internetFailure = `
              <div style="font-size:34px; text-align:center; margin-top:40px;">Please check your internet connection and try again.
